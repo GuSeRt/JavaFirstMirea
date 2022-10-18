232cd1cd667d616ac5d330373d9ef8fd8657efb7
GuSeRt <gusert34@yandex.ru>
2022-10-19 01:31:08 +0300

    task 8

---
+++ src/ru/mirea/task8/OneAndZero.java
@@ -0,0 +1,18 @@
+package ru.mirea.task8;
+
+public class OneAndZero {
+    public int getOneZero(int a, int b) {
+        if (a == 0 & b != 0) {
+            return 1;
+        }
+        else if (a == 1 & b != 0) {
+            return b + 1;
+        }
+        else if (a > 1 & b == 0) {
+            return 0;
+        }
+        else {
+            return getOneZero(a - 1, b - 1) + getOneZero(a, b - 1);
+        }
+    }
+}
